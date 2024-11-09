dcece28458f3f17dfec3ea7b1d6efe732f8fa656
f11 <eieioeiji0501@gmail.com>
2024-11-09 12:55:30 +0900

    [remove]remove update.bash

--- update.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd debian/jammy/
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
-apt-ftparchive release . > Release
-gpg --default-key "eieioeiji0501@gmail.com" -abs -o - Release > Release.gpg
-gpg --default-key "eieioeiji0501@gmail.com" --clearsign -o - Release > InRelease
-cd ../../
